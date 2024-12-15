cb88c27a93488ccb8104ae1dc6e08beedde8ee8d
EngAHmed2025 <elshafeyahmed110@gmail.com>
2024-12-15 22:00:30 +0200

    PutSomeInterFaces

---
+++ BookingSystem/BookingSystem.Core/Interfaces/IHallService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingSystem.Core.Models;
+
+namespace BookingSystem.Core.Interfaces
+{
+    public interface IHallService
+    {
+        IEnumerable<Hall> GetAllHalls();
+        void AddHall(Hall hall);
+         Hall GetHallById(int id);
+    }
+}
---
+++ BookingSystem/BookingSystem.Core/Interfaces/IbookingServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingSystem.Core.Models;
+
+namespace BookingSystem.Core.Interfaces
+{
+    public interface IbookingServices
+    {
+        IEnumerable<DateTime> GetAvailableDates();
+        Booking GetBookingById(int bookingId);
+        void CreateBooking(Booking model);
+        void CancelBooking(int bookingId);
+        Contract GenerateContract(int bookingId);
+        void SendBookingConfirmation(int bookingId);
+    }
+}
