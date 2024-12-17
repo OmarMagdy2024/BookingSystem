23a503484b22f3e350c7d372ee577aeeaaa3eb82
EngAHmed2025 <elshafeyahmed110@gmail.com>
2024-12-17 21:05:22 +0200

    trytoCreatetherole

---
+++ BookingSystem/BookingSystem.API/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using BookingSystem.Core.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace BookingSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser>userManager , SignInManager<IdentityUser>signInManager)
+        {
+         _userManager = userManager;
+           _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AccountController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]Register registers)
+        {
+            var user = new IdentityUser(userName = registers.Username, Email = registers.Email);
+            var result = await _userManager.CreateAsync(user, registers.Password);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
